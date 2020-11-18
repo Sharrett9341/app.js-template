0c3da31db557741e387229c009a833740e5676aa
“[Chris <Sharrett9341@gmail.com>
2020-11-18 15:10:26 -0500

    Basic list Using EJS

--- app.js
+++ app.js
@@ -5,49 +5,39 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+var items = ["Buy Food", "Cook", "Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+      extended: true
+}));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+      var today = new Date();
 
-  switch (expression) {
-      case 0:
-          day = "Sunday";
-          break;
-    case 1:
-          day = "Monday";
-          break;
-    case 2:
-          day = "Tuesday";
-          break;
-    case 3:
-          day = "Wednesday";
-          break;
-    case 4:
-          day = "Thursday";
-          break;
-     case 5:
-            day = "Friday";
-            break;
-       case 6:
-            day = "Saturday";
-            break;
-           default:
-           console.log("Error: current day is equal to " + currentDay);
-    
-  }
+      var options = {
+            weekday: "long",
+            day: "numeric",
+            month: "long"
+      };
 
-    res.render("list", {
-          kindOfDay: day
-      });
+      var day = today.toLocaleDateString("en-US", options);
+
+
+      res.render("list", {kindOfDay: day,newListItems: items});
 
 });
 
+app.post("/", function (req, res) {
+      item = req.body.newItem; 
 
+      items.push(item);
+
+      res.redirect("/");
+});
 
 app.listen(3000, function () {
-    console.log("Server Started on Port 3000");
+      console.log("Server Started on Port 3000");
 });
